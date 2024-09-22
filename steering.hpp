260b20874cdf20d0f36ea56396894e0e46a3025c
catharsys <74600209+catharsys64@users.noreply.github.com>
2024-09-22 17:00:27 +0200

    Update steering.hpp

--- steering.hpp
+++ steering.hpp
@@ -15,17 +15,17 @@ int scanPatterns() {
 	uintptr_t steering_base = patterns::scan(gta5, "E8 ? ? ? ? 48 63 87 ? ? ? ? 48 8B 8F");
 	getEntityAddy = (uint64_t(*)(int))patterns::scan(gta5, "83 f9 ff 74 31 4c 8b 0d ? ? ? ? 44 8b c1 49 8b 41 08");
 	
-	uintptr_t steering_control = patterns::scan(gta5, "F3 44 0F 11 87 ? ? ? ? F6 80 ? ? ? ? ? 74 4D"); // SA OVIM JE SIZE 9
-	uintptr_t steering_parked = patterns::scan(gta5, "44 89 BB ? ? ? ? 8B 0D ? ? ? ? 0F 57 C0 81 F9 ? ? ? ? 74 ?"); // SA OVIM JE SIZE 7
+	uintptr_t steering_control = patterns::scan(gta5, "F3 44 0F 11 87 ? ? ? ? F6 80 ? ? ? ? ? 74 4D");
+	uintptr_t steering_parked = patterns::scan(gta5, "44 89 BB ? ? ? ? 8B 0D ? ? ? ? 0F 57 C0 81 F9 ? ? ? ? 74 ?");
 
 	if (steering_base != NULL && getEntityAddy != NULL && steering_control != NULL && steering_parked != NULL) {
 		steering_angle_offset = *(uint32_t*)(steering_base + 23);
 
 		BYTE bytes[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
 		SteeringControl = new MemPatcher(steering_control, 9, bytes);
-		SteeringParked = new MemPatcher(steering_parked, 7, bytes); // mozemo isti niz bajtova jer on uzima samo onoliko koliko je naveden m_size, a ne ceo prosledjeni niz
+		SteeringParked = new MemPatcher(steering_parked, 7, bytes);
 		
-		SteeringParked->Patch(); // ne moramo non stop nego samo sad, i posle kad unloadujemo Restore() i tjt
+		SteeringParked->Patch();
 
 		return 1;
 	}
